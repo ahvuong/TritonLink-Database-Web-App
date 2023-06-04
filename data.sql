b8e200b0a45e5dcbb5f57d33cb871ac6ff4a0dc8
Avuong <avuong23@student.cccd.edu>
2023-06-04 00:13:38 -0700

    Add delete trigger for CPQG

--- data.sql
+++ data.sql
@@ -761,58 +761,56 @@ CREATE TABLE CPQG AS
     FROM past_classes
     GROUP BY title, year, quarter, instructor_name;
 
-------------------------CPQG Trigger-----------------------
+------------------------Insert Or Update CPQG Trigger-----------------------
 DROP TRIGGER IF EXISTS insert_update_CPQG_trigger ON past_classes;
 DROP FUNCTION IF EXISTS insert_update_CPQG_trigger();
 
 CREATE OR REPLACE FUNCTION insert_update_CPQG_trigger() RETURNS TRIGGER AS $insert_update_CPQG_trigger$
 BEGIN
-  -- Update the count of grades in CPQG for the corresponding course, professor, and quarter
-  
-  IF NOT EXISTS (SELECT 1 
-                FROM CPQG
-                WHERE CPQG.title = NEW.title
-                    AND CPQG.year = NEW.year
-                    AND CPQG.quarter = NEW.quarter
-                    AND CPQG.instructor_name =  NEW.instructor_name)
-  THEN
-       INSERT INTO CPQG(title, year, quarter, instructor_name, count_A, count_B, count_C, count_D, count_other)
-    VALUES (
-        NEW.title, NEW.year, NEW.quarter, NEW.instructor_name,
-        (CASE WHEN NEW.grade IN ('A+', 'A', 'A-') THEN 1 ELSE 0 END),
-        (CASE WHEN NEW.grade IN ('B+', 'B', 'B-') THEN 1 ELSE 0 END),
-        (CASE WHEN NEW.grade IN ('C+', 'C', 'C-') THEN 1 ELSE 0 END),
-        (CASE WHEN NEW.grade IN ('D+', 'D', 'D-') THEN 1 ELSE 0 END),
-        (CASE WHEN NEW.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN 1 ELSE 0 END)
-    );
- ELSE
-  	UPDATE CPQG
-    SET
-      count_A = CASE WHEN OLD.grade IN ('A+', 'A', 'A-') THEN count_A - 1 ELSE count_A END,
-      count_B = CASE WHEN OLD.grade IN ('B+', 'B', 'B-') THEN count_B - 1 ELSE count_B END,
-      count_C = CASE WHEN OLD.grade IN ('C+', 'C', 'C-') THEN count_C - 1 ELSE count_C END,
-      count_D = CASE WHEN OLD.grade IN ('D+', 'D', 'D-') THEN count_D - 1 ELSE count_D END,
-      count_other = CASE WHEN OLD.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN count_other - 1 ELSE count_other END
-    WHERE title = OLD.title
-        AND year = OLD.year
-        AND quarter = OLD.quarter
-        AND instructor_name = OLD.instructor_name;
-        
-    UPDATE CPQG
-    SET
-      count_A = CASE WHEN NEW.grade IN ('A+', 'A', 'A-') THEN count_A + 1 ELSE count_A END,
-      count_B = CASE WHEN NEW.grade IN ('B+', 'B', 'B-') THEN count_B + 1 ELSE count_B END,
-      count_C = CASE WHEN NEW.grade IN ('C+', 'C', 'C-') THEN count_C + 1 ELSE count_C END,
-      count_D = CASE WHEN NEW.grade IN ('D+', 'D', 'D-') THEN count_D + 1 ELSE count_D END,
-      count_other = CASE WHEN NEW.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN count_other + 1 ELSE count_other END
-    WHERE title = NEW.title
-        AND year = NEW.year
-        AND quarter = NEW.quarter
-        AND instructor_name = NEW.instructor_name;
 
-    
-  END IF;
-  RETURN NEW;
+    IF NOT EXISTS (SELECT 1 
+                    FROM CPQG
+                    WHERE CPQG.title = NEW.title
+                        AND CPQG.year = NEW.year
+                        AND CPQG.quarter = NEW.quarter
+                        AND CPQG.instructor_name =  NEW.instructor_name)
+    THEN
+        INSERT INTO CPQG(title, year, quarter, instructor_name, count_A, count_B, count_C, count_D, count_other)
+        VALUES (
+            NEW.title, NEW.year, NEW.quarter, NEW.instructor_name,
+            (CASE WHEN NEW.grade IN ('A+', 'A', 'A-') THEN 1 ELSE 0 END),
+            (CASE WHEN NEW.grade IN ('B+', 'B', 'B-') THEN 1 ELSE 0 END),
+            (CASE WHEN NEW.grade IN ('C+', 'C', 'C-') THEN 1 ELSE 0 END),
+            (CASE WHEN NEW.grade IN ('D+', 'D', 'D-') THEN 1 ELSE 0 END),
+            (CASE WHEN NEW.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN 1 ELSE 0 END)
+        );
+    ELSE
+        UPDATE CPQG
+        SET
+        count_A = CASE WHEN OLD.grade IN ('A+', 'A', 'A-') THEN count_A - 1 ELSE count_A END,
+        count_B = CASE WHEN OLD.grade IN ('B+', 'B', 'B-') THEN count_B - 1 ELSE count_B END,
+        count_C = CASE WHEN OLD.grade IN ('C+', 'C', 'C-') THEN count_C - 1 ELSE count_C END,
+        count_D = CASE WHEN OLD.grade IN ('D+', 'D', 'D-') THEN count_D - 1 ELSE count_D END,
+        count_other = CASE WHEN OLD.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN count_other - 1 ELSE count_other END
+        WHERE title = OLD.title
+            AND year = OLD.year
+            AND quarter = OLD.quarter
+            AND instructor_name = OLD.instructor_name;
+            
+        UPDATE CPQG
+        SET
+        count_A = CASE WHEN NEW.grade IN ('A+', 'A', 'A-') THEN count_A + 1 ELSE count_A END,
+        count_B = CASE WHEN NEW.grade IN ('B+', 'B', 'B-') THEN count_B + 1 ELSE count_B END,
+        count_C = CASE WHEN NEW.grade IN ('C+', 'C', 'C-') THEN count_C + 1 ELSE count_C END,
+        count_D = CASE WHEN NEW.grade IN ('D+', 'D', 'D-') THEN count_D + 1 ELSE count_D END,
+        count_other = CASE WHEN NEW.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN count_other + 1 ELSE count_other END
+        WHERE title = NEW.title
+            AND year = NEW.year
+            AND quarter = NEW.quarter
+            AND instructor_name = NEW.instructor_name;
+        
+    END IF;
+    RETURN NEW;
 END;
 
 $insert_update_CPQG_trigger$ LANGUAGE plpgsql;
@@ -821,7 +819,48 @@ $insert_update_CPQG_trigger$ LANGUAGE plpgsql;
 CREATE TRIGGER insert_update_CPQG_trigger
 AFTER INSERT OR UPDATE ON past_classes
 FOR EACH ROW
-EXECUTE FUNCTION  insert_update_CPQG_trigger();
+EXECUTE FUNCTION insert_update_CPQG_trigger();
+
+------------------------Insert Or Update CPQG Trigger-----------------------
+DROP TRIGGER IF EXISTS delete_CPQG_trigger ON past_classes;
+DROP FUNCTION IF EXISTS delete_CPQG_trigger();
+
+CREATE OR REPLACE FUNCTION delete_CPQG_trigger() RETURNS TRIGGER AS $delete_CPQG_trigger$
+BEGIN
+
+    UPDATE CPQG
+    SET
+    count_A = CASE WHEN OLD.grade IN ('A+', 'A', 'A-') THEN count_A - 1 ELSE count_A END,
+    count_B = CASE WHEN OLD.grade IN ('B+', 'B', 'B-') THEN count_B - 1 ELSE count_B END,
+    count_C = CASE WHEN OLD.grade IN ('C+', 'C', 'C-') THEN count_C - 1 ELSE count_C END,
+    count_D = CASE WHEN OLD.grade IN ('D+', 'D', 'D-') THEN count_D - 1 ELSE count_D END,
+    count_other = CASE WHEN OLD.grade NOT IN ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-') THEN count_other - 1 ELSE count_other END
+    WHERE title = OLD.title
+        AND year = OLD.year
+        AND quarter = OLD.quarter
+        AND instructor_name = OLD.instructor_name;
+
+    RETURN NEW;
+END;
+
+$delete_CPQG_trigger$ LANGUAGE plpgsql;
+
+-- Create the trigger to invoke the update_CPQG function upon insertion or update of grades
+CREATE TRIGGER delete_CPQG_trigger
+AFTER DELETE ON past_classes
+FOR EACH ROW
+EXECUTE FUNCTION delete_CPQG_trigger();
+
+
+------------------------CPQG TEST CASES-----------------------
+INSERT INTO past_classes(student_id, section_id, title, year, quarter, instructor_name, grade, units, grade_conversion,class_type) 
+VALUES('21', '3', 'CSE142', '2021', 'FA', 'Flo_Rence', 'B-', '4','3.7', 'lower_units');
+
+UPDATE past_classes
+SET grade = 'C+'
+WHERE student_id = '21' AND section_id = '3';
+
+DELETE FROM past_classes WHERE student_id = '21' AND section_id = '3';
 
 ------------------------Materialized View CPG-----------------------
 
@@ -830,5 +869,3 @@ EXECUTE FUNCTION  insert_update_CPQG_trigger();
 SELECT trigger_name 
 FROM information_schema.triggers;
 
-INSERT INTO past_classes(student_id, section_id, title, year, quarter, instructor_name, grade, units, grade_conversion,class_type) 
-VALUES('21', '3', 'CSE142', '2021', 'FA', 'Flo_Rence', 'B-', '4','3.7', 'lower_units');
